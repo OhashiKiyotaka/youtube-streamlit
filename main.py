dbd4d9db2b067a6dac81bb2b977db2c51991c811
Kiyotaka <kiyotaka6525@gmail.com>
2022-01-19 21:23:32 +0900

    1st commit

--- main.py
+++ main.py
@@ -28,13 +28,3 @@ expander2 = st.expander('問い合わせ2')
 expander2.write('問い合わせ2の回答')
 expander3 = st.expander('問い合わせ3')
 expander3.write('問い合わせ3の回答')
-# text = st.text_input('あなたの趣味を教えてください。')
-# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
-
-
-# 'あなたの趣味:', text
-# 'コンディション:', condition
-
-# if st.checkbox('Show Image'):
-#    img = Image.open('sample.jpg')
-#    st.image(img, caption='Cat', use_column_width=True)
